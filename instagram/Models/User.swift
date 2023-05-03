c286fc39bab84133508b2f52b4043a2f7fc564d3
Parkjju <rudwns3927@gmail.com>
2023-05-03 17:34:13 +0900

    feat: User 임시모델 생성

---
+++ instagram/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  instagram
+//
+//  Created by 박경준 on 2023/05/03.
+//
+
+import Foundation
+
+class User{
+    var username: String?
+    var name: String?
+    var profileImageUrl: String?
+    var uid: String?
+}
