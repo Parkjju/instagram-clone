5a46ef802c50a6296f833560e6a88457e1d4ec7c
Parkjju <rudwns3927@gmail.com>
2023-05-03 16:42:17 +0900

    feat: 오토레이아웃 확장코드 생성

--- instagram/Utils/Extensions.swift
+++ instagram/Utils/Extensions.swift
@@ -5,4 +5,35 @@
 //  Created by 박경준 on 2023/05/03.
 //
 
-import Foundation
+import UIKit
+
+extension UIView{
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, paddingBottom:CGFloat, width: CGFloat, height: CGFloat){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if width != 0{
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0{
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
