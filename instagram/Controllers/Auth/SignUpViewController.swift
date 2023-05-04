4be107f2e7fd42e435c4f384b017dfc76fda3179
Parkjju <rudwns3927@gmail.com>
2023-05-04 12:37:01 +0900

    feat: 회원가입 뷰 - 컨트롤러 연결

---
+++ instagram/Controllers/Auth/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  instagram
+//
+//  Created by 박경준 on 2023/05/03.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view = SignUpView()
+    }
+}
