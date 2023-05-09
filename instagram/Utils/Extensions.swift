2b604e6cabc120356e24664dc13bd851604fd7ab
Parkjju <rudwns3927@gmail.com>
2023-05-10 08:57:48 +0900

    feat: UIImage extension 함수 정의 - 이미지 타겟사이즈에 따라 비율조정 - stretching 문제 예방을 위해 scaleFactor 활용 / UIGraphicsImageRenderer로 이미지 리사이징

--- instagram/Utils/Extensions.swift
+++ instagram/Utils/Extensions.swift
@@ -38,6 +38,34 @@ extension UIView{
     }
 }
 
+// MARK: Image resizing
+extension UIImage{
+    func newScaleOfImage(targetSize: CGSize) -> CGSize{
+        // 가로세로 스케일링 비율 측정
+        let widthScaleRatio = targetSize.width / self.size.width
+        let heightScaleRatio = targetSize.height / self.size.height
+        
+        // 가로세로 스케일링 비율 중 더 큰쪽에 맞추기
+        let scaleFactor = min(widthScaleRatio, heightScaleRatio)
+        
+        // 이미지 리사이징시 비율은 맞추되, 오토레이아웃에 설정해둔 height값 기준으로 크기를 최대높이를 조절한다
+        return CGSize(width: self.size.width * scaleFactor, height: 100)
+    }
+    
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage{
+        
+        // 새로운 스케일 계산
+        let scaledSize = newScaleOfImage(targetSize: targetSize)
+        let renderer = UIGraphicsImageRenderer(size: scaledSize)
+        
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: scaledSize))
+        }
+        
+        return scaledImage
+    }
+}
+
 // MARK: 제스처 간단하게 추가하는 코드
 final class BindableGestureRecognizer: UITapGestureRecognizer {
     private var action: () -> Void
