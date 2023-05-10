4570bee8088d146ae49c51ea38afc900d7dee2c4
Parkjju <rudwns3927@gmail.com>
2023-05-11 08:26:17 +0900

    feat: cropImage 익스텐션 함수 파라미터 타입 변경 / 이미지 틀 previewImageContainerView로 변경

--- instagram/Views/Utils/CustomPickerView.swift
+++ instagram/Views/Utils/CustomPickerView.swift
@@ -94,9 +94,29 @@ class CustomPickerView: UIView {
         parentVC.dismiss(animated: true)
     }
     
+    // MARK: 완료버튼 클릭시 CustomView에 있는 이미지를 SignUpVC의 이미지에 넣어줘야함.
     @objc func handleCompletionButtonTapped(){
-        guard let parentVC = self.parentViewController as? CustomPickerViewController else { return }
+        // 1. parentViewController 속성을 통해 customVC에 접근
+        guard let customPickerVC = self.parentViewController as? CustomPickerViewController else { return }
         
-        parentVC.dismiss(animated: true)
+        // 2. 로그인 뷰컨과 회원가입 뷰컨을 임베딩한 네비게이션 컨트롤러 - presetingVC 속성 접근으로 인해 네비게이션을 참조하게됨
+        let embededInLoginAndSignupVC = self.parentViewController?.presentingViewController as! UINavigationController
+        
+        // 3. 네비게이션 컨트롤러 맨 마지막 컨트롤러는 signupVC
+        let signupVC = embededInLoginAndSignupVC.viewControllers.last as! SignUpViewController
+        
+        // 4. signupVC의 뷰에 접근하여 이미지뷰 디폴트 값인 avatar.png를 customVC의 이미지로 교체해주는 작업
+        let signupView = signupVC.view as! SignUpView
+        let imageView = self.previewImageContainerView.subviews.first as! UIImageView
+        let signupViewProfileImageView = signupView.profileImageContainerView.subviews.first as! UIImageView
+        
+        // 5. 코너 부여 및 이미지 리사이징 - 해상도 깨짐 방지
+        
+        signupViewProfileImageView.layer.cornerRadius = 50
+        signupViewProfileImageView.image = cropImage(sourceImage: imageView.image!, view: previewImageContainerView).scalePreservingAspectRatio(targetSize: CGSize(width: 100, height: 100), autoResize: false)
+        
+        
+        
+        customPickerVC.dismiss(animated: true)
     }
 }
