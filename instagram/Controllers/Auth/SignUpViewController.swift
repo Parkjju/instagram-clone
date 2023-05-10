1d94ed47f9a6ad7e1f11617bc9ebff26a8f327dc
Parkjju <rudwns3927@gmail.com>
2023-05-10 18:30:48 +0900

    feat: 회원가입 VC 커스텀 이미지 피커뷰 컨트롤러 modelPresentationStyle fullScreen 추가

--- instagram/Controllers/Auth/SignUpViewController.swift
+++ instagram/Controllers/Auth/SignUpViewController.swift
@@ -17,6 +17,7 @@ class SignUpViewController: UIViewController {
         self.view = signupView
         signupView.delegate = self
         signupView.pickerDelegate = self
+        
     }
 }
 
@@ -34,48 +35,10 @@ extension SignUpViewController: GestureDelegate{
 }
 
 extension SignUpViewController: PickerDelegate{
-    func setupPickerView(picker: CustomPHPickerViewController) {
-        picker.delegate = self
+    func setupPickerView(picker: CustomPickerViewController) {
+        picker.modalPresentationStyle = .fullScreen
         present(picker, animated: true)
     }
 }
 
-extension SignUpViewController: PHPickerViewControllerDelegate{
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        
-        guard let signupView = self.view as? SignUpView else {return}
-        
-        // 이미지뷰 요소에서 UIImageView 타입캐스팅으로 요소 꺼내오기
-        let imageView = signupView.profileImageContainerView.subviews.first as! UIImageView
-        // 코너 부여
-        imageView.layer.cornerRadius = 50
-        imageView.layer.borderWidth = 2
-        imageView.layer.borderColor = UIColor.lightGray.cgColor
-        
-        picker.dismiss(animated: true)
-        
-        // picker dimiss후에 비동기 처리 필요 - indicator 추가
-        let itemProvider = results.first?.itemProvider
-        
-        if let itemProvider = itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self){
-            
-            itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
-                
-                // picker로 선택된 이미지 UIImage로 타입캐스팅
-                guard let image = image as? UIImage else {return}
-                
-                // UIImage extension에 구현해둔 함수 scalePreservingAspectRatio호출
-                // 타겟사이즈 설정 - height는 최대 100값을 갖지만, 비율은 그대로 계산한다
-                // image stretching 문제 예방을 위함!
-                let scaledImage =  image.scalePreservingAspectRatio(targetSize: CGSize(width: 150, height: 150))
-                
-                // 메인쓰레드로 비동기작업 결과물 보내서 이미지 교체
-                DispatchQueue.main.async {
-                    imageView.image = scaledImage
-                }
-            }
-        } else {
-            print("Image Load Error")
-        }
-    }
-}
+
