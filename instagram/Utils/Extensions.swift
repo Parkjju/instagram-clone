b276b7fe8416a31d24e7a57e71ef6a56b47789f5
Parkjju <rudwns3927@gmail.com>
2023-05-21 00:07:32 +0900

    feat: 드래그 셀렉터 예외처리 (경계값 로직 개선)

--- instagram/Utils/Extensions.swift
+++ instagram/Utils/Extensions.swift
@@ -47,6 +47,7 @@ extension UIImage{
         let heightScaleRatio = targetSize.height / self.size.height
         
         // 가로세로 스케일링 비율 중 더 큰쪽에 맞추기
+        // 현재 사이즈가 타겟 사이즈보다 작아야, 타겟 사이즈에 맞추려고 할 것 -> min함수
         let scaleFactor = min(widthScaleRatio, heightScaleRatio)
         
         // 이미지 리사이징시 비율은 맞추되, 오토레이아웃에 설정해둔 height값 기준으로 크기를 최대높이를 조절한다
@@ -136,15 +137,9 @@ extension UIImageView{
         // 이동량을 더하는 형태 -> 계속 누적되어 목표값을 넘어서게됨
         // 직전의 translate값을 저장
         // 변화하는 상태값을 계속 트래킹하며 이미지뷰 origin위치를 변경해줘야함
-        if(sender.view!.transform.a > 1 || sender.view!.transform.d > 1){
-            sender.view!.center.x += sender.translation(in: sender.view).x
-            sender.view!.center.y += sender.translation(in: sender.view).y
-            
-            sender.setTranslation(.zero, in: sender.view)
-        }else{
-            // 얘는 성능문제가 발생하지 않나
-            sender.view?.frame.origin = sender.translation(in: sender.view)
-        }
+        sender.view!.center.x += sender.translation(in: sender.view).x
+        sender.view!.center.y += sender.translation(in: sender.view).y
+        sender.setTranslation(.zero, in: sender.view)
         
         if(sender.state == .ended){
             checkImageOriginIsZero(sender)
@@ -152,7 +147,7 @@ extension UIImageView{
     }
     
     func checkImageOriginIsZero(_ sender: UIPanGestureRecognizer){
-        
+        // 너비 변환이 이루어졌을때
         if(sender.view!.transform.a != 1){
             // 프레임이 왼쪽경계보다 안쪽으로 당겨질때
             if(sender.view!.frame.origin.x > 0){
@@ -176,17 +171,37 @@ extension UIImageView{
             }
             
             // 프레임이 아래쪽 경계보다 안쪽으로 당겨질때 - 이미지 wrapper UIView 높이로 체크
-            if(sender.view!.frame.maxY < sender.view!.superview!.frame.height){
+            if(sender.view!.frame.maxY < sender.view!.frame.width){
+                
                 UIView.animate(withDuration: 0.3) {
                     sender.view!.center.y += (sender.view!.superview!.frame.height - sender.view!.frame.maxY)
                 }
             }
             
-        }else{
-            if(sender.view!.frame.origin.x > 0 || sender.view!.frame.origin.x < 0 || sender.view!.frame.origin.y > 0){
+        }
+        // 아핀변환 없이 스케일링이 잘 맞춰졌을때
+        else{
+            if(sender.view!.frame.origin.x < 0){
                 UIView.animate(withDuration: 0.3) {
-                    sender.view?.frame.origin.x = 0
-                    sender.view?.frame.origin.y = 0
+                    sender.view!.center.x = sender.view!.frame.width / 2
+                }
+            }
+            
+            if(sender.view!.frame.maxX > sender.view!.frame.width){
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.x = sender.view!.frame.width / 2
+                }
+            }
+            
+            if(sender.view!.frame.maxY < sender.view!.frame.width){
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.y += sender.view!.frame.width - sender.view!.frame.maxY
+                }
+            }
+            
+            if(sender.view!.frame.minY > 0){
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.y -=  sender.view!.frame.minY
                 }
             }
         }
@@ -195,16 +210,29 @@ extension UIImageView{
     }
     
     func checkImageOriginIsZero(_ sender: UIPinchGestureRecognizer){
-        if(sender.view!.frame.origin.x > 0 || sender.view!.frame.origin.y > 0){
+        if(sender.view!.frame.origin.x > 0 || sender.view!.frame.origin.y > 0 || sender.view!.frame.origin.x < 0 || sender.view!.frame.maxY < sender.view!.frame.width){
+            
             let imageView = sender.view as! UIImageView
+            
             UIView.animate(withDuration: 0.3) {
-                sender.view!.transform.a = 1
-                sender.view!.transform.d = 1
+                // PinchGesture 종료 후 너비가 높이보다 큰 경우 ratio계산 후 그 이하로는 이미지 스케일링 더 진행되지 않도록 고정
+                // CustomPickerViewController 컬렉션뷰 didSelect 프로토콜 메서드에서 이미지뷰 아핀변환값 지정하는 부분 체크
+                if(imageView.frame.height < imageView.frame.width){
+                    let ratio = imageView.frame.width / imageView.frame.height
+                    
+                    imageView.transform.a = ratio
+                    imageView.transform.d = ratio
+                }else{
+                    imageView.transform.a = 1
+                    imageView.transform.d = 1
+                }
                 
-                sender.view!.frame.origin.x = 0
-                sender.view!.frame.origin.y = 0
+                sender.view!.center.x = sender.view!.superview!.frame.width / 2
+                sender.view!.center.y = sender.view!.superview!.frame.height / 2
             }
         }
+        
+        
     }
     
     func enableZoom(){
